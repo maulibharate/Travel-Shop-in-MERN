fde69575962c2f26afb053dcd0a5a76f3e2753f1
maulibharate <maulibharate96@gmail.com>
2021-11-27 09:51:39 +0530

    change 2.0

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: `http://localhost:5000`,
+            target: `https://travel-shop-in-mern.herokuapp.com`,
             changeOrigin: true,
         })
     );
